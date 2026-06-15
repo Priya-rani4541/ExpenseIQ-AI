6d51c1e4c0f0507ce845c023f0486291675f18b0
Priya Rani <priyaranikumari819@gmail.com>
2026-06-15 05:40:48 +0530

    deployment settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!*i0t8ej@qo96c^_qz53l17bnzo5u5td5v)0rhx0(2b=hpvend'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
